46d65c34bd50faea2f47f9a19d757eb2cd6471ab
mashharuki <avp.104.106.107@ae.auone-net.jp>
2026-09-21 18:53:16 +0900

    update

--- pkgs/facilitator/src/config.ts
+++ pkgs/facilitator/src/config.ts
@@ -1,7 +1,8 @@
 import { kairos } from "viem/chains";
 
 // chain info
+// Please replace this with your own chain info if you are using a different chain.
 export const chainInfo = {
   chain: kairos,
-  chainId: "eip155:1001",
+  chainId: `eip155:${kairos.id}`,
 };
